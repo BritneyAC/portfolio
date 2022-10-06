d875fcebdea76fa9677828ba4e6f0c7c7ffb482f
BriteyAC <BritneyArtemis@gmail.com>
2022-10-05 23:40:50 -0700

    style fixes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const withImages = require("next-images")
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: { 
+    images: {
+      allowFutureImage: true,
+    }
+  },
+  
 }
 
-module.exports = nextConfig
+module.exports = withImages(nextConfig)
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/css/Gallery.module.css"
-import Image from "next/image"
+import Image from "next/future/image"
 import Demo from "@/previews/idleitaireDemo.gif"
 import CarverVo from "@/previews/carvervo.png"
 
@@ -8,13 +8,18 @@ export default function Gallery(){
     <div className={styles.gallery}>
       <div className={styles.project}>
         <a href="https://www.idleitaire.com"><h2>Idleitaire</h2>
-        <Image src={Demo} alt="preview of idleitaire.com" quality={75}/></a>
+        <div>
+          <Image src={"/previews/idleitaireDemo.gif"} fill alt="preview of idleitaire.com" quality={75}/>
+        </div></a>
         <p>A version of solitaire made into a idle game.</p>
         <a href="https://github.com/BritneyAC/idleitaire">Github</a>
       </div>
       <div className={styles.project}>
         <a href="https://www.carvervo.com"><h2>Carver VO</h2>
-        <Image src={CarverVo} alt="preview of carvervo.com"/></a>
+        <div>
+          <Image src={"/previews/carvervo.png"} fill alt="preview of carvervo.com"/>
+        </div>
+        </a>
         <p>Portfolio website for my friend that is a narrorator</p>
         <a href="https://github.com/BritneyAC/talia-narration">Github</a>
       </div>
